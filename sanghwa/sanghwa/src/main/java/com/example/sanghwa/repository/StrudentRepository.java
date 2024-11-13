0e525e26c174e442aa23c5d37ef0ec760eb985f7
ralph <sanghwaj222@gmail.com>
2024-11-13 10:17:55 +0900

    new

---
+++ sanghwa/sanghwa/src/main/java/com/example/sanghwa/repository/StrudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.sanghwa.repository;
+
+import com.example.sanghwa.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StrudentRepository extends JpaRepository<Student, Integer> {
+}
